5f3ef19907cf003aab722fd4e9d0596d476563ac
yakirLE <yakir.elkan@gmail.com>
2021-09-01 17:03:23 +0300

    Convert YAML to POJO

---
+++ design-patterns/src/main/java/com/yakir/yml/ConvertYAML.java
@@ -0,0 +1,59 @@
+package com.yakir.yml;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class ConvertYAML {
+
+	public static void main(String[] args) {
+		System.out.println(getYAML());
+		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+		mapper.findAndRegisterModules();
+		try {
+			Order order = mapper.readValue(getYAML(), Order.class);
+			System.out.println(order.toString());
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static final String getYAML() {
+		return "orderNo: A001\n"
+			 + "date: 2019-04-17\n"
+			 + "customerName: Customer, Joe\n"
+			 + "orderLines:\n"
+			 + "    - item: No. 9 Sprockets\n"
+			 + "      quantity: 12\n"
+			 + "      unitPrice: 1.23\n"
+			 + "    - item: Widget (10mm)\n"
+			 + "      quantity: 4\n"
+			 + "      unitPrice: 3.45";
+	}
+	
+	@Data
+	@NoArgsConstructor
+	public static class Order {
+		private String orderNo;
+	    private LocalDate date;
+	    private String customerName;
+	    private List<OrderLine> orderLines;
+	}
+	
+	@Data
+	@NoArgsConstructor
+	public static class OrderLine {
+	    private String item;
+	    private int quantity;
+	    private BigDecimal unitPrice;
+
+	    // Constructors, Getters, Setters and toString
+	}
+}
